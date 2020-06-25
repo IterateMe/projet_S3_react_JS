129366eb74afadb6227f8528877bdb72c9e3fc3d
bouboul122 <boul0902@usherbrooke.ca>
2020-06-25 14:43:02 -0400

    Mini conflicts resolved

---
+++ src/Route/chooseCourses/StudentChooseCourse.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CourseResults from '../../Components/CourseResults/CourseResults';
+import Zeus from '../../util/Zeus.js';
+
+class StudentChooseCourse extends React.Component{
+    
+}
+
+export default StudentChooseCourse;
