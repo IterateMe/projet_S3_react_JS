7b62b673926e3d521b9b6619d199fb230d4fc9e3
Victor <vicfreve@gmail.com>
2020-07-06 15:36:04 -0400

    MatchCoUrSe

---
+++ src/Components/MatchCourse/MatchCourse.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class MatchCourse extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="MatchCourse">
+                <div className="course-information">
+                    <h3>{this.props.course.id}</h3>
+                    <p>{this.props.course.student} | {this.props.course.mentor}</p>
+                </div>
+            </div>
+        )
+    }
+}
