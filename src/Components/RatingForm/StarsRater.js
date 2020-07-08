f0273a4a7387be5cad293df92e50afb5a21ac70a
Victor <vicfreve@gmail.com>
2020-07-08 13:24:33 -0400

    FormRating de student finis

--- src/Components/RatingForm/StarsRater.js
+++ src/Components/RatingForm/StarsRater.js
@@ -6,7 +6,8 @@ class StarsRater extends React.Component {
         super(props);
         this.state = {
             currentRating: 1,
-            maxRating: 5
+            maxRating: 5,
+            selectedRating: 1
         }
         this.star = process.env.PUBLIC_URL + '/images/filled_star.png'
         this.emptyStar = process.env.PUBLIC_URL + '/images/empty_star.png'
@@ -14,8 +15,22 @@ class StarsRater extends React.Component {
 
     starClicked(event) {
         event.preventDefault()
+        this.props.onChange(this.props.id, event.target.id)
+        this.setState({
+            currentRating: event.target.id,
+            selectedRating: event.target.id
+        });
+    }
+
+    onMouseEnter(event) {
         this.setState({
             currentRating: event.target.id
+        });
+    }
+
+    onMouseLeave(event) {
+        this.setState({
+            currentRating: this.state.selectedRating
         });
     }
 
@@ -23,14 +38,19 @@ class StarsRater extends React.Component {
         let ratingBar = [];
         for (let i = 1; i <= this.state.maxRating; i++) {
             ratingBar.push(
-                <img height='20dp' width='20dp' alt="etoile" id={i} onClick={this.starClicked.bind(this)} src={
-                    i <= this.state.currentRating ? this.star : this.emptyStar
-                }
+                <img height='20dp' width='20dp' alt="etoile" id={i}
+                     onClick={this.starClicked.bind(this)}
+                     onMouseEnter={this.onMouseEnter.bind(this)}
+                     onMouseLeave={this.onMouseLeave.bind(this)}
+                     src={
+                         i <= this.state.currentRating ? this.star : this.emptyStar
+                     }
                 />
             );
         }
         return (
             <div>
+                <h4>{this.props.label}</h4>
                 {ratingBar}
             </div>
         )
